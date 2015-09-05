49216dea0d351a3e3bcbf73871f36172878486fe
古川信行 <t_sakura@m1.ffn.ne.jp>
2015-09-06 01:19:28 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var newsFeed = require('./news-feed.js');
+//console.log( newsFeed );
+
+//コールバック
+var fnc = function( feeds ){
+	var meta = feeds.meta;
+	var item;
+	while (item = feeds.read()) {
+		console.log("---");
+		//日付を取得
+		var d = new Date(item['dc:date']['#']);
+		var yymmdd = d.getFullYear()+"年" + (d.getMonth()+1)+"月"+d.getDate()+"日";
+		console.log( yymmdd );
+		console.log( item.title );
+		/*
+		//TODO ここで日付とタイトルだけBOCCOに送信
+		var msg = yymmdd+" "+item.title;
+		bocco.postMessageText("ぼっこにメッセージを送るよ",function(json){
+		
+		});
+		*/
+	}
+};
+
+//例) はてなブックマーク テクノロジー
+var rss_url = "http://feeds.feedburner.com/hatena/b/hotentry";
+newsFeed.getFeed(rss_url,fnc);
